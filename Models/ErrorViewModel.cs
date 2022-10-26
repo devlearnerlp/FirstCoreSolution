44282356e343936c7543466c7da177630eb7af2b
kpraghav <kpraghav@gmail.com>
2022-10-26 18:02:22 +0530

    first core soln

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FirstCoreSolution.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
